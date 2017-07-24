cd7c016495916f97064682fce8c83ff31182d0ff
sanshenghui <lovewsic@gmail.com>
2017-07-24 11:21:17 +0800

    字符串工具类

--- scriptCodec/src/main/java/com/sanshengshui/util/base/StringUtil.java
+++ scriptCodec/src/main/java/com/sanshengshui/util/base/StringUtil.java
@@ -1,4 +1,35 @@
 package com.sanshengshui.util.base;
 
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @ClassName 字符串工具类
+ * @author 穆书伟
+ * @description 字符串工具类
+ * @date 2017/7/24 11:20:15
+ */
 public class StringUtil {
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String str) {
+        if (str != null) {
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否非空
+     */
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+    /**
+     * 分割固定格式的字符串
+     */
+    public static String[] split(String str, String separator) {
+        return StringUtils.splitByWholeSeparator(str, separator);
+    }
 }
