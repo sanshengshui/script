16374fa550a774951cd962228b4b3de7f3dd9887
sanshenghui <lovewsic@gmail.com>
2017-07-24 12:34:41 +0800

    utils

---
+++ scriptCodec/src/main/java/com/sanshengshui/util/extend/Charsets.java
@@ -0,0 +1,18 @@
+package com.sanshengshui.util.extend;
+
+import java.nio.charset.Charset;
+
+/**
+ * @ClassName Charsets
+ * @Description 字符集GBK
+ * @author 穆书伟
+ * @date 2017年7月24日 上午12:18:33
+ */
+public class Charsets {
+    // 字符集GBK
+    public static final Charset GBK = Charset.forName("GBK");
+    // 字符集ISO-8859-1
+    public static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
+    // 字符集utf-8
+    public static final Charset UTF_8 = Charset.forName("UTF-8");
+}
