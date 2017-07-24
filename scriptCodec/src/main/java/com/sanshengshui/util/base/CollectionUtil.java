7746afb037d567846d1b8207a7d6bede70399973
sanshenghui <lovewsic@gmail.com>
2017-07-24 11:16:30 +0800

    集合工具类和序列化工具类(基于Protostuff实现)

--- scriptCodec/src/main/java/com/sanshengshui/util/base/CollectionUtil.java
+++ scriptCodec/src/main/java/com/sanshengshui/util/base/CollectionUtil.java
@@ -1,4 +1,43 @@
 package com.sanshengshui.util.base;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.MapUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @ClassName CollectionUtil
+ * @author 穆书伟
+ * @description 集合工具类
+ * @date 2017/7/24 10:52:49
+ */
 public class CollectionUtil {
+    /**
+     * 判断 Collection 是否为空
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return CollectionUtils.isEmpty(collection);
+    }
+
+    /**
+     * 判断 Collection 是否非空
+     */
+    public static boolean isNotEmpty(Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    /**
+     * 判断 Map 是否为空
+     */
+    public static boolean isEmpty(Map<?, ?> map) {
+        return MapUtils.isEmpty(map);
+    }
+
+    /**
+     * 判断 Map 是否非空
+     */
+    public static boolean isNotEmpty(Map<?, ?> map) {
+        return !isEmpty(map);
+    }
 }
