c055f04b94809d486ecee87c8f8eb57f7c78df26
sanshenghui <lovewsic@gmail.com>
2017-07-25 15:54:32 +0800

    RPC编码器

--- scriptCodec/src/main/java/com/sanshengshui/codec/RpcEncoder.java
+++ scriptCodec/src/main/java/com/sanshengshui/codec/RpcEncoder.java
@@ -1,10 +1,29 @@
 package com.sanshengshui.codec;
 
+import com.sanshengshui.util.base.SerializationUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
 /**
  * @ClassName RpcEncoder
  * @author 穆书伟
  * @description RPC编码器
  * @date 2017/7/23 15:20:08
  */
-public class RpcEncoder {
+public class RpcEncoder extends MessageToByteEncoder {
+    private Class<?> genericClass;
+
+    public RpcEncoder(Class<?> genericClass){
+        this.genericClass = genericClass;
+    }
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, Object o, ByteBuf byteBuf)
+            throws Exception {
+        if(genericClass.isInstance(o)){
+            byte[] data = SerializationUtil.serialize(o);
+            byteBuf.writeInt(data.length);
+            byteBuf.writeBytes(data);
+        }
+    }
 }
