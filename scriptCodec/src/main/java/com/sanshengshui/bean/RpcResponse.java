1ded4bb9c2bc1512fff84329c9421f8507105f26
sanshenghui <lovewsic@gmail.com>
2017-07-25 14:21:10 +0800

    封装RPC响应

--- scriptCodec/src/main/java/com/sanshengshui/bean/RpcResponse.java
+++ scriptCodec/src/main/java/com/sanshengshui/bean/RpcResponse.java
@@ -7,5 +7,31 @@ package com.sanshengshui.bean;
  * @date 2017/7/23 15:23:02
  */
 public class RpcResponse {
+    private String requestId;
+    private Exception exception;
+    private Object result;
 
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
 }
