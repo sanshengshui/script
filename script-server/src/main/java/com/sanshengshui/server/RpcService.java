9a3f2ccda685089d714b59d53a76d8a528b90b28
sanshenghui <lovewsic@gmail.com>
2017-07-28 16:46:03 +0800

    RpcService

--- script-server/src/main/java/com/sanshengshui/server/RpcService.java
+++ script-server/src/main/java/com/sanshengshui/server/RpcService.java
@@ -1,4 +1,28 @@
 package com.sanshengshui.server;
 
-public class RpcService {
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @ClassName RpcService
+ * @author 穆书伟
+ * @Description RPC服务注册(标注在服务实现类上)
+ * @Date 2017年7月28日 下午16:41:53
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface RpcService {
+    /**
+     * 服务接口类
+     */
+    Class<?> value();
+    /**
+     *服务版本号
+     */
+    String version() default "";
 }
